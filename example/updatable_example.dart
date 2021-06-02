658ac46c5deaf45a0e5fc00efd9872aaa1a9c79d
Fernando Rodríguez <frr@keepcoding.io>
2021-06-02 10:31:13 +0200

    configure to begin

--- example/updatable_example.dart
+++ example/updatable_example.dart
@@ -1,6 +1,5 @@
 import 'package:updatable/updatable.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  // Do something
 }
--- lib/updatable.dart
+++ lib/updatable.dart
@@ -3,6 +3,6 @@
 /// More dartdocs go here.
 library updatable;
 
-export 'src/updatable_base.dart';
+export 'src/updatable_mixin.dart';
 
 // TODO: Export any libraries intended for clients of this package.
